0bccb917ec6ea07d039156f9d5b5a403861c2d7a
Ilya Tzoop <ilyatzoop@gmail.com>
2022-08-02 14:54:48 +0500

    Added tls support

---
+++ Vostok.ClusterClient.Singular/Tls/ClusterConfigThumbprintsProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Vostok.ClusterConfig.Client.Abstractions;
+using Vostok.Singular.Core.Tls;
+
+namespace Vostok.Clusterclient.Singular.Tls
+{
+    internal class ClusterConfigThumbprintsProvider : IThumbprintsProvider
+    {
+        private readonly IClusterConfigClient clusterConfigClient;
+        private readonly ClusterConfigPath thumbprintsPath;
+
+        public ClusterConfigThumbprintsProvider(IClusterConfigClient clusterConfigClient, ClusterConfigPath thumbprintsPath)
+        {
+            this.clusterConfigClient = clusterConfigClient;
+            this.thumbprintsPath = thumbprintsPath;
+        }
+
+        public IEnumerable<string> GetWhitelist()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetBlacklist()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
