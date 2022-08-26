2f9bda45bcd5dcb1783abfe8a1cf4b622cffeb72
Ilya Tzoop <ilyatzoop@gmail.com>
2022-08-26 15:02:24 +0500

    Cut scheme and port

---
+++ Vostok.ClusterClient.Singular/Helpers/SingularClusterConfigClusterProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vostok.Clusterclient.Core.Topology;
+using Vostok.Clusterclient.Topology.CC;
+using Vostok.Commons.Collections;
+
+namespace Vostok.Clusterclient.Singular.Helpers;
+
+internal class SingularClusterConfigClusterProvider : IClusterProvider
+{
+    private readonly ClusterConfigClusterProvider clusterConfigClusterProvider;
+    private readonly CachingTransform<IList<Uri>, Uri[]> transform;
+
+    public SingularClusterConfigClusterProvider(ClusterConfigClusterProvider clusterConfigClusterProvider)
+    {
+        this.clusterConfigClusterProvider = clusterConfigClusterProvider;
+        transform = new CachingTransform<IList<Uri>, Uri[]>(cluster => cluster.Select(CutSchemeAndPort).ToArray());
+    }
+
+    public IList<Uri> GetCluster() => transform.Get(clusterConfigClusterProvider.GetCluster());
+
+    private static Uri CutSchemeAndPort(Uri uri)
+    {
+        var cleanUri = uri.GetComponents(UriComponents.AbsoluteUri & ~UriComponents.Port & ~UriComponents.Scheme, UriFormat.UriEscaped);
+        return new Uri(cleanUri);
+    }
+}
