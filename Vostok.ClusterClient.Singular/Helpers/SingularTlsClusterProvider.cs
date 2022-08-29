df3908de7641a3629cdadac241f502581cb14283
Ilya Tzoop <ilyatzoop@gmail.com>
2022-08-29 11:29:19 +0500

    Changed public interface so that user could specify whether he wants to use TLS or not

---
+++ Vostok.ClusterClient.Singular/Helpers/SingularTlsClusterProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vostok.Clusterclient.Core.Topology;
+using Vostok.Commons.Collections;
+
+namespace Vostok.Clusterclient.Singular.Helpers;
+
+internal class SingularTlsClusterProvider : IClusterProvider
+{
+    private readonly IClusterProvider clusterConfigClusterProvider;
+    private readonly CachingTransform<IList<Uri>, Uri[]> transform;
+
+    public SingularTlsClusterProvider(IClusterProvider clusterConfigClusterProvider)
+    {
+        this.clusterConfigClusterProvider = clusterConfigClusterProvider;
+        transform = new CachingTransform<IList<Uri>, Uri[]>(cluster => cluster.Select(ReplaceSchemeAndPort).ToArray());
+    }
+
+    public IList<Uri> GetCluster() => transform.Get(clusterConfigClusterProvider.GetCluster());
+
+    private static Uri ReplaceSchemeAndPort(Uri uri)
+    {
+        return new UriBuilder(uri)
+        {
+            Scheme = Uri.UriSchemeHttps,
+            Port = 443
+        }.Uri;
+    }
+}
